9b479f73589f2c0293398cabaf700f76410483ed
Nadir Benallal <nadir.benallal@heig-vd.ch>
2021-10-28 14:08:09 +0200

    IFCViewer canvas takes all its container space

--- frontend/src/components/IFCViewer.vue
+++ frontend/src/components/IFCViewer.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="fill-height">
   <input
       type="file"
       name="load"
@@ -41,8 +41,9 @@ export default {
 
 <style scoped>
 #scene3d {
-  width: 700px;
-  height: 700px;
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 
 .file-input {
