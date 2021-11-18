95bc61da43dadaa2fa5a95a38bdf5ffdb671956c
Nadir Benallal <nadir.benallal@heig-vd.ch>
2021-11-18 15:37:21 +0100

    Add simple /ifc route

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
+const ifcRouter = require("./routes/ifc");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/ifc', ifcRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
