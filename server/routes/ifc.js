4d4aad73d219ab362ab508f8be10dcd24d95ce1b
Nadir Benallal <nadir.benallal@heig-vd.ch>
2021-12-08 10:17:55 +0100

    Setting up example IFC file generation from tomvandig ifc-server examples

--- server/routes/ifc.js
+++ server/routes/ifc.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const {ifcWriter} = require("../services/ifc/ifcWriter");
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.send('Hello IFC!');
+  const ifcString = ifcWriter.writeIFC();
+  res.send(ifcString);
 });
 
 module.exports = router;
