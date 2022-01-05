240b1f495d481a1214b9fef96338e8a373bc6b42
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-01-05 14:20:42 +0100

    Do not deconstruct WebIFC

--- frontend/src/services/ifcWriter.js
+++ frontend/src/services/ifcWriter.js
@@ -1,16 +1,4 @@
 import WebIFC from "web-ifc";
-const {
-    IfcAxis2Placement2D, IFCAXIS2PLACEMENT2D,
-    IfcAxis2Placement3D, IFCAXIS2PLACEMENT3D,
-    IfcCartesianPoint, IFCCARTESIANPOINT,
-    IfcCircleProfileDef, IFCCIRCLEPROFILEDEF,
-    IfcColumn, IFCCOLUMN,
-    IfcDirection, IFCDIRECTION,
-    IFCEXTRUDEDAREASOLID, IfcExtrudedAreaSolid,
-    IfcLocalPlacement, IFCLOCALPLACEMENT,
-    IfcProfileTypeEnum
-} = WebIFC;
-
 
 let EID = 1;
 
@@ -42,8 +30,8 @@ function enm(v)
 function Point(model, api, o)
 {
     const ID = EID++;
-    const pt = new IfcCartesianPoint(ID,
-        IFCCARTESIANPOINT,
+    const pt = new WebIFC.IfcCartesianPoint(ID,
+        WebIFC.IFCCARTESIANPOINT,
         [real(o.x), real(o.y), real(o.z)]);
     api.WriteLine(model, pt);
     return ref(ID);
@@ -52,8 +40,8 @@ function Point(model, api, o)
 function Dir(model, api, o)
 {
     const ID = EID++;
-    const pt = new IfcDirection(ID,
-        IFCDIRECTION,
+    const pt = new WebIFC.IfcDirection(ID,
+        WebIFC.IFCDIRECTION,
         [real(o.x), real(o.y), real(o.z)]);
     api.WriteLine(model, pt);
     return ref(ID);
@@ -62,8 +50,8 @@ function Dir(model, api, o)
 function Point2D(model, api, o)
 {
     const ID = EID++;
-    const pt = new IfcCartesianPoint(ID,
-        IFCCARTESIANPOINT,
+    const pt = new WebIFC.IfcCartesianPoint(ID,
+        WebIFC.IFCCARTESIANPOINT,
         [real(o.x), real(o.y)]);
     api.WriteLine(model, pt);
     return ref(ID);
@@ -73,8 +61,8 @@ function AxisPlacement(model, api, o)
 {
     const locationID = Point(model, api, o);
     const ID = EID++;
-    const pt = new IfcAxis2Placement3D(ID,
-        IFCAXIS2PLACEMENT3D,
+    const pt = new WebIFC.IfcAxis2Placement3D(ID,
+        WebIFC.IFCAXIS2PLACEMENT3D,
         locationID,
         empty(),
         empty());
@@ -86,8 +74,8 @@ function AxisPlacement2D(model, api, o)
 {
     const locationID = Point2D(model, api, o);
     const ID = EID++;
-    const pt = new IfcAxis2Placement2D(ID,
-        IFCAXIS2PLACEMENT2D,
+    const pt = new WebIFC.IfcAxis2Placement2D(ID,
+        WebIFC.IFCAXIS2PLACEMENT2D,
         locationID,
         empty());
     api.WriteLine(model, pt);
@@ -98,8 +86,8 @@ function Placement(model, api, o)
 {
     const axisID = AxisPlacement(model, api, o);
     const ID = EID++;
-    const pt = new IfcLocalPlacement(ID,
-        IFCLOCALPLACEMENT,
+    const pt = new WebIFC.IfcLocalPlacement(ID,
+        WebIFC.IFCLOCALPLACEMENT,
         empty(),
         axisID);
     api.WriteLine(model, pt);
@@ -109,9 +97,9 @@ function Placement(model, api, o)
 function CircleProfile(model, api, rad, o)
 {
     const ID = EID++;
-    const pt = new IfcCircleProfileDef(ID,
-        IFCCIRCLEPROFILEDEF,
-        enm(IfcProfileTypeEnum.AREA),
+    const pt = new WebIFC.IfcCircleProfileDef(ID,
+        WebIFC.IFCCIRCLEPROFILEDEF,
+        enm(WebIFC.IfcProfileTypeEnum.AREA),
         str('column-prefab'),
         AxisPlacement2D(model, api, o),
         real(rad));
@@ -122,8 +110,8 @@ function CircleProfile(model, api, rad, o)
 function ExtrudedAreaSolid(model, api, pos, dir, rad, len)
 {
     const ID = EID++;
-    const pt = new IfcExtrudedAreaSolid(ID,
-        IFCEXTRUDEDAREASOLID,
+    const pt = new WebIFC.IfcExtrudedAreaSolid(ID,
+        WebIFC.IFCEXTRUDEDAREASOLID,
         CircleProfile(model, api, rad, { x: 0, y: 0 }),
         AxisPlacement(model, api, pos),
         Dir(model, api, dir),
@@ -141,8 +129,8 @@ function StandardColumn(model, api, pos)
         2);
 
     const ID = EID++;
-    const pt = new IfcColumn(ID,
-        IFCCOLUMN,
+    const pt = new WebIFC.IfcColumn(ID,
+        WebIFC.IFCCOLUMN,
         str("GUID"),
         empty(),
         str("name"),
