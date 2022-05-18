76f69440e93be927dde60c745aa6a85b9aa2c767
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-05-18 15:24:45 +0200

    Simple creation of a file entry in the database

--- server/src/main/java/ch/heig/examples/bim/FileResource.java
+++ server/src/main/java/ch/heig/examples/bim/FileResource.java
@@ -3,11 +3,14 @@ package ch.heig.examples.bim;
 import ch.heig.examples.bim.dtos.FileDto;
 import ch.heig.examples.bim.entities.FileEntity;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.util.Date;
 
 @Path("/files")
@@ -16,7 +19,13 @@ public class FileResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public FileEntity create(FileDto dto) {
-        return new FileEntity(1, dto.name(), new Date());
+    @Transactional
+    public Response create(FileDto dto) {
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.name = dto.name();
+        fileEntity.creationDate = new Date();
+
+        fileEntity.persist();
+        return Response.created(URI.create("/files/" + fileEntity.id)).build();
     }
 }
