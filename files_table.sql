7c85a648a6f451ba30ca39e837b6b1deb67ab8d3
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-05-18 10:45:00 +0200

    Replace file path by file blob in database

--- files_table.sql
+++ files_table.sql
@@ -3,7 +3,7 @@ USE `bim-examples`;
 CREATE TABLE IF NOT EXISTS `files` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
-  `path` VARCHAR(255) NOT NULL,
+  `ifc_file` BLOB,
   `creation_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY `pk_id`(`id`)
 );
