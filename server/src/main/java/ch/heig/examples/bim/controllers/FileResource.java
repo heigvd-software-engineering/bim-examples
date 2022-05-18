c459bde7ee6e953694db020de90af70f2142b3ab
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-05-18 16:58:36 +0200

    Use file service class to create entity

--- server/src/main/java/ch/heig/examples/bim/controllers/FileResource.java
+++ server/src/main/java/ch/heig/examples/bim/controllers/FileResource.java
@@ -1,8 +1,7 @@
 package ch.heig.examples.bim.controllers;
 
 import ch.heig.examples.bim.dtos.FileDto;
-import ch.heig.examples.bim.entities.FileEntity;
-import ch.heig.examples.bim.repositories.FileRepository;
+import ch.heig.examples.bim.services.FileService;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -12,25 +11,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.net.URI;
-import java.util.Date;
 
 @Path("/files")
 public class FileResource {
 
     @Inject
-    FileRepository fileRepository;
+    FileService fileService;
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
     public Response create(FileDto dto) {
-        FileEntity entity = new FileEntity();
-        entity.name = dto.name();
-        entity.creationDate = new Date();
-
-        fileRepository.persist(entity);
-        return Response.created(URI.create("/files/" + entity.id)).build();
+        fileService.createEntity(dto);
+        return Response.status(Response.Status.CREATED).build();
     }
 }
