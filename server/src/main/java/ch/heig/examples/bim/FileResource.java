21549099f71313dc5251d65373a10b282e25ee8b
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-05-18 11:56:14 +0200

    Simple example of POST HTTP request on the /files endpoint

--- server/src/main/java/ch/heig/examples/bim/FileResource.java
+++ server/src/main/java/ch/heig/examples/bim/FileResource.java
@@ -1,16 +1,22 @@
 package ch.heig.examples.bim;
 
-import javax.ws.rs.GET;
+import ch.heig.examples.bim.dtos.FileDto;
+import ch.heig.examples.bim.entities.FileEntity;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Date;
 
 @Path("/files")
 public class FileResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String create() {
-        return "Hello on the files endpoint";
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public FileEntity create(FileDto dto) {
+        return new FileEntity(1, dto.name(), new Date());
     }
 }
