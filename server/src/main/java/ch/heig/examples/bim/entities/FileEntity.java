1e4286f27ee7451e6ec01d8992950c026dd3fc2a
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-05-18 11:57:20 +0200

    Remove Serializable

--- server/src/main/java/ch/heig/examples/bim/entities/FileEntity.java
+++ server/src/main/java/ch/heig/examples/bim/entities/FileEntity.java
@@ -1,6 +1,5 @@
 package ch.heig.examples.bim.entities;
 
-import java.io.Serializable;
 import java.util.Date;
 
-public record FileEntity(int id, String name, Date creationDate) implements Serializable {}
+public record FileEntity(int id, String name, Date creationDate) {}
