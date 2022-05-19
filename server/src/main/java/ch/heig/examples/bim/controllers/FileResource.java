8e1149920b19d6e531066e1cacf834997522faa0
Nadir Benallal <nadir.benallal@heig-vd.ch>
2022-05-19 12:37:15 +0200

    Add endpoint to add blob to a file

--- server/src/main/java/ch/heig/examples/bim/controllers/FileResource.java
+++ server/src/main/java/ch/heig/examples/bim/controllers/FileResource.java
@@ -1,15 +1,12 @@
 package ch.heig.examples.bim.controllers;
 
-import ch.heig.examples.bim.services.FileService;
 import ch.heig.examples.bim.dtos.FileDto;
 import ch.heig.examples.bim.entities.FileEntity;
+import ch.heig.examples.bim.services.FileService;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
@@ -27,5 +24,15 @@ public class FileResource {
     public Response create(FileDto dto) {
         FileEntity entity = fileService.createEntity(dto);
         return Response.created(URI.create("/files/" + entity.id)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+    @Produces()
+    @Transactional
+    public Response update(long id, byte[] fileBlob) {
+        fileService.updateFileBlob(id, fileBlob);
+        return Response.noContent().build();
     }
 }
