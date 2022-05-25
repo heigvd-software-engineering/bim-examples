e30e0e47314bb8634d4444fd0b8e1f6e65f38ac4
Nadir Benallal <brofmaster@gmail.com>
2022-05-25 17:26:33 +0200

    Add post method to http client

--- frontend/src/httpClient.js
+++ frontend/src/httpClient.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
 class HttpClient {
+
     /**
      * @param {string} url
      */
     async get(url) {
         return axios.get(url);
+    }
+
+    /**
+     * @param {string} url
+     * @param {any} body
+     */
+    async post(url, body) {
+        return axios.post(url, body);
     }
 }
 
