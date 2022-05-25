b349b8891473439af373554e7392a7f149fb0fc6
Nadir Benallal <brofmaster@gmail.com>
2022-05-25 16:52:47 +0200

    Add findById for ifc resource

--- frontend/src/services/ifcResourceService.js
+++ frontend/src/services/ifcResourceService.js
@@ -1,9 +1,25 @@
 import { httpClient } from "@/httpClient";
 
+const baseUrl = "http://localhost:8080/files";
+
+/**
+ *
+ * @return {Promise<AxiosResponse<any>>}
+ */
 async function findAllSummaries() {
-    return httpClient.get("http://localhost:8080/files");
+    return httpClient.get(baseUrl);
+}
+
+/**
+ *
+ * @param {number} id
+ * @return {Promise<AxiosResponse<any>>}
+ */
+async function findById(id) {
+    return httpClient.get(`${baseUrl}/${id}`);
 }
 
 export {
     findAllSummaries,
+    findById,
 };
